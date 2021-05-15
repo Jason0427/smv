deb72a48b8a8be0846d4f8c0fc51674705cac425
Jason0427 <13732909893@163.com>
2021-05-15 23:13:02 +0800

    Initial commit

---
+++ src/main/java/com/cn/springshared/smv/ApiToMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.cn.springshared.smv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiToMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiToMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cn/springshared/smv/ApiToMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.springshared.smv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiToMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
